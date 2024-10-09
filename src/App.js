251f09db5342fac3d7460b91f26064626f6cc04f
DineshDhanoki <167537800+DineshDhanoki@users.noreply.github.com>
2024-10-09 23:36:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Add from './components/Add'
+
+export default function App() {
+  return (
+    <div>
+      <h1> Hello Diplo </h1>
+      <h1><Add/></h1>
+    </div>
+    
+      )
+}
+
---
+++ src/components/Add.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Add() {
+  return (
+    <div>
+      Hey My name is Diplo
+    </div>
+  )
+}
+
