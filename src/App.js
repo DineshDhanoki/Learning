1f961071a9bd6ddb91b3e301ff2b6f6c93d2b780
DineshDhanoki <167537800+DineshDhanoki@users.noreply.github.com>
2024-10-15 20:13:27 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from 'react'
-import { createContext } from 'react'
-import Friendship from './components/Count'
-import Movieshow from './components/Movieticket'
-import Moviechange from './components/Movie'
-import Name from './components/Name'
-import Title from './components/Title'
-import ComponentA from './components/ComponentA'
+import Counter from './components/Counter'
 
-export const Data = createContext()
+const App = () => {
 
-const Information = () => {
-  const naam = "DIPLO"
-}
-
-export default function App() {
   return (
-
-    <div>
-      <Data.Provider value={naam}>
-      <ComponentA />
-      </Data.Provider>
-      <Friendship />
-      <Movieshow />
-      <Moviechange />
-      <Name />
-      <Title />
-    </div>
+    <>
+    <Counter />
+    </>
   )
 }
+
+export default App
